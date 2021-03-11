cd868e25e31f88049986466d37d4b15fe77992e6
kuba <30964181+kubabub@users.noreply.github.com>
2021-03-11 10:31:07 +0100

    Update itunescover.py

--- itunescover.py
+++ itunescover.py
@@ -2,11 +2,10 @@ from requests import get
 import re
 from json import loads
 r=re.compile("https?:\/\/music.apple.com/[^/]+\/[^/]+\/[^/]+\/([0-9]+)")
-def get_album_json(id, cc=us):
-    return loads(get("https://itunes.apple.com/lookup?id={}&country={}}&limit=25".format(id, cc)).text)['results'][0]
-id=input('id or link: ')
+def get_album_cover(id, cc='us'):
+    return loads(get("https://itunes.apple.com/lookup?id={}&country={}}&limit=25".format(id, cc)).text)['results'][0]['artworkUrl100'][:-13]+'100000x100000-999.jpg'
+id=input('id or URL: ')
 if not isinstance(id, int):
     id=r.findall(id)[0]
-cv=get_album_json(id)
-cover=cv['artworkUrl100'][:-13]+'100000x100000-999.jpg'
+cover=get_album_cover(id)
 print(cover)
